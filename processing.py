a04e463fa856ee0d10eef68066dd6d7d7a8995d2
Pedro Sader Azevedo <pedro.saderazevedo@lnbr.cnpem.br>
2025-04-20 13:50:35 -0300

    processing: add a script for processing and saving the raw data

---
+++ processing.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from pathlib import Path
+
+DATA_DIRECTORY: Path = Path("data")
+RESULTS_DIRECTORY: Path = DATA_DIRECTORY / Path("2025-04-15T23-22-45") / Path("raw")
+PROCESSED_DATA_DIRECTORY: Path = DATA_DIRECTORY / Path("2025-04-15T23-22-45") / Path("processed")
+
+TABLE_PREFIXES = ["affiliations", "authorships", "documents"]
+
+for prefix in TABLE_PREFIXES:
+    # Find all matching authorship files
+    tsv_files = sorted(RESULTS_DIRECTORY.glob(f"{prefix}*.tsv"))
+
+    # Load and concatenate all files
+    df = pd.concat((pd.read_csv(f, sep='\t', dtype=str) for f in tsv_files), ignore_index=True)
+
+    if prefix == "authorships":
+        # Split and explode affiliations
+        df['affiliations'] = df['affiliations'].str.split(',')
+        df = df.explode('affiliations').reset_index(drop=True)
+
+    # Save to a single merged file
+    df.to_csv(PROCESSED_DATA_DIRECTORY / f"{prefix}.tsv", sep='\t', index=False)

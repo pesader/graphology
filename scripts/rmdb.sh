5520e8a4c1e7f0f819288c387f208c145d556f10
Pedro Sader Azevedo <pedro.saderazevedo@lnbr.cnpem.br>
2025-05-12 17:31:01 -0300

    scripts/rmdb: add notice to run script with privilege

--- scripts/rmdb.sh
+++ scripts/rmdb.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ "$EUID" -ne 0 ]; then
+  echo "Please, run this script with privilege"
+  exit 1
+fi
+
 rm -rf /var/lib/neo4j/data/databases/neo4j && rm -rf /var/lib/neo4j/data/transactions/neo4j

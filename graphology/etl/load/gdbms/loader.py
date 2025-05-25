a5783c3791068c544decd62b5ada970d2c2e5e78
Pedro Sader Azevedo <pedro.saderazevedo@lnbr.cnpem.br>
2025-05-25 16:03:27 -0300

    refactor(gdbms/loader): overhaul gdmbs loader to use neo4j-admin

--- graphology/etl/load/gdbms/loader.py
+++ graphology/etl/load/gdbms/loader.py
@@ -1,5 +1,6 @@
 import uuid
 import logging
+import subprocess
 
 from .database import driver
 from graphology import log
@@ -9,101 +10,29 @@ class GDBMSLoader:
     def __init__(self, timestamp: str):
         self.timestamp: str = timestamp
 
-    def _populate_institutions(self):
-        file_url = f"file:///{self.timestamp}/institutions.tsv"
-        with driver.session() as session:
-            session.run(
-                """
-                LOAD CSV WITH HEADERS FROM $file_url AS row FIELDTERMINATOR '\t'
-                MERGE (i:Institution {scopus_id: row.scopus_id})
-                SET i += row;
-                """,
-                {
-                    "file_url": file_url,
-                },
-            )
+    def _run_neo4j_admin(self) -> None:
+        """Runs a bash command and returns its output."""
+        print('The "neo4j-admin" command requires privilege to run.')
+        command = f"""
+        sudo neo4j-admin database import full \
+          --delimiter="\t" \
+          --nodes=Author=data/{self.timestamp}/neo4j/node_authors.tsv \
+          --nodes=Document=data/{self.timestamp}/neo4j/node_documents.tsv \
+          --nodes=Institution=data/{self.timestamp}/neo4j/node_institutions.tsv \
+          --nodes=Authorship=data/{self.timestamp}/neo4j/node_authorships.tsv \
+          --relationships=INVOLVES_AUTHOR=data/{self.timestamp}/neo4j/rel_authorship_author.tsv \
+          --relationships=INVOLVES_DOCUMENT=data/{self.timestamp}/neo4j/rel_authorship_document.tsv \
+          --relationships=INVOLVES_INSTITUTION=data/{self.timestamp}/neo4j/rel_authorship_institution.tsv \
+          --relationships=COLLABORATED_WITH=data/{self.timestamp}/neo4j/rel_author_author.tsv \
+          --overwrite-destination \
+          --verbose
+        """
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        log(logging.INFO, self.timestamp, result.stdout)
 
-        log(
-            logging.INFO,
-            self.timestamp,
-            f"finished populating institution nodes",
-        )
-
-    def _populate_authors(self):
-        file_url = f"file:///{self.timestamp}/authors.tsv"
-        with driver.session() as session:
-            session.run(
-                """
-                LOAD CSV WITH HEADERS FROM $file_url AS row FIELDTERMINATOR '\t'
-                MERGE (a:Author {scopus_id: row.scopus_id})
-                SET a.name = row.name;
-                """,
-                {
-                    "file_url": file_url,
-                },
-            )
-
-        log(
-            logging.INFO,
-            self.timestamp,
-            f"finished populating author nodes",
-        )
-
-    def _populate_documents(self):
-        file_url = f"file:///{self.timestamp}/documents.tsv"
-        with driver.session() as session:
-            session.run(
-                """
-                LOAD CSV WITH HEADERS FROM $file_url AS row FIELDTERMINATOR '\t'
-                MERGE (d:Document {scopus_id: row.scopus_id})
-                SET d += row;
-                """,
-                {
-                    "file_url": file_url,
-                },
-            )
-
-        log(
-            logging.INFO,
-            self.timestamp,
-            f"finished populating document nodes",
-        )
-
-    def _populate_authorships(self):
-        file_url = f"file:///{self.timestamp}/authorships.tsv"
-        with driver.session() as session:
-            session.run(
-                """
-                LOAD CSV WITH HEADERS FROM $file_url AS row FIELDTERMINATOR '\t'
-                CREATE (authorship:Authorship {
-                    id: $authorship_id,
-                    first_author: row.first_author
-                })
-                WITH authorship, row
-                MATCH (a:Author {scopus_id: row.author_id}),
-                      (d:Document {scopus_id: row.document_id}),
-                      (i:Institution {scopus_id: row.institution_id})
-                MERGE (authorship)-[:INVOLVES_AUTHOR]->(a)
-                MERGE (authorship)-[:INVOLVES_DOCUMENT]->(d)
-                MERGE (authorship)-[:INVOLVES_INSTITUTION]->(i)
-                """,
-                {
-                    "file_url": file_url,
-                    "authorship_id": str(uuid.uuid4()),
-                },
-            )
-
-        log(
-            logging.INFO,
-            self.timestamp,
-            f"finished populating authorship nodes",
-        )
+        if result.returncode != 0:
+            log(logging.ERROR, self.timestamp, result.stderr)
+            raise Exception("Unable to populate neo4j database")
 
     def load(self):
-        # Independent nodes
-        self._populate_institutions()
-        self._populate_authors()
-        self._populate_documents()
-
-        # Associative node
-        self._populate_authorships()
+        self._run_neo4j_admin()
